81a1c94dd90b2b2d27cfb25e8b2b9ae70cf215f6
oussamach <chati.oussama@gmail.com>
2018-01-15 11:20:40 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,368 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblplayer = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Score = New System.Windows.Forms.Label()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(-4, 94)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(221, 170)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "-"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Button2.Location = New System.Drawing.Point(223, 94)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(221, 170)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "-"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Button3.Location = New System.Drawing.Point(451, 94)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(221, 170)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "-"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Button4.Location = New System.Drawing.Point(-4, 270)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(221, 170)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "-"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Button5.Location = New System.Drawing.Point(223, 270)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(221, 170)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "-"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Button6.Location = New System.Drawing.Point(451, 270)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(221, 170)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "-"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Button7.Location = New System.Drawing.Point(-4, 446)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(221, 170)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "-"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Button8.Location = New System.Drawing.Point(223, 446)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(221, 170)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "-"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.Button9.Location = New System.Drawing.Point(451, 446)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(221, 170)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "-"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(90, 43)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(103, 17)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Player VS Player"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(223, 43)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(119, 17)
+        Me.RadioButton3.TabIndex = 11
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Player VS Comptuer"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(451, 13)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckBox1.Size = New System.Drawing.Size(33, 17)
+        Me.CheckBox1.TabIndex = 12
+        Me.CheckBox1.Text = "X"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(451, 37)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(34, 17)
+        Me.CheckBox2.TabIndex = 13
+        Me.CheckBox2.Text = "O"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(359, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Player 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(359, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Player 2"
+        '
+        'lblplayer
+        '
+        Me.lblplayer.AutoSize = True
+        Me.lblplayer.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblplayer.ForeColor = System.Drawing.Color.Orange
+        Me.lblplayer.Location = New System.Drawing.Point(247, 63)
+        Me.lblplayer.Name = "lblplayer"
+        Me.lblplayer.Size = New System.Drawing.Size(0, 20)
+        Me.lblplayer.TabIndex = 16
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(565, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Score"
+        '
+        'Score
+        '
+        Me.Score.AutoSize = True
+        Me.Score.Location = New System.Drawing.Point(636, 14)
+        Me.Score.Name = "Score"
+        Me.Score.Size = New System.Drawing.Size(0, 13)
+        Me.Score.TabIndex = 18
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(32, 13)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 19
+        Me.Button10.Text = "Reset"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(767, 105)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 20
+        Me.Label4.Text = "Player  1 Win"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(770, 137)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(67, 13)
+        Me.Label5.TabIndex = 21
+        Me.Label5.Text = "Player 2 Win"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(773, 168)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(32, 13)
+        Me.Label6.TabIndex = 22
+        Me.Label6.Text = "Draw"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(856, 105)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(10, 13)
+        Me.Label7.TabIndex = 23
+        Me.Label7.Text = "."
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(859, 137)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(10, 13)
+        Me.Label8.TabIndex = 24
+        Me.Label8.Text = "."
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(859, 168)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(10, 13)
+        Me.Label9.TabIndex = 25
+        Me.Label9.Text = "."
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(737, 12)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(45, 13)
+        Me.Label10.TabIndex = 26
+        Me.Label10.Text = "Label10"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1012, 635)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Score)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblplayer)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Reset"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblplayer As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Score As Label
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,391 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Label10.Text = CStr(1 + 1)
+        ' Label10.Text = CStr(CInt(Label10.Text) + 1)
+        Label7.Text = CStr(0)
+        Label8.Text = CStr(0)
+        ' btnunchecked()
+    End Sub
+    Dim player As Integer = 0
+    Dim word As String
+    Dim random As New Random
+    Dim i As String
+    Dim msgwin As String = "you win "
+    Dim plname As String = ""
+    Dim comptescore As Integer = 0
+    Dim status As Boolean
+
+    Public Sub MsgWinName(plwinname As String)
+        MsgBox("you win :" + plwinname)
+    End Sub
+    Public Sub playerwin(pl As Integer)
+
+        If pl = 1 Then
+            plname = "player 1"
+            comptescore += 1
+            'Label7.Text = CStr(comptescore)
+            Label7.Text = CStr(CInt(Label7.Text) + 1)
+        ElseIf pl = 2 Then
+            plname = "Player 2"
+            comptescore += 1
+            'Label8.Text = CStr(comptescore)
+            Label8.Text = CStr(CInt(Label8.Text) + 1)
+        End If
+
+    End Sub
+    Public Sub btnunchecked()
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+    End Sub
+    Public Sub checkwin()
+        '1 2 3
+        '4 5 6
+        '7 8 9
+        ' if  status two player don't win 
+        ' if player win 
+        ' If status = False Then
+        If Button1.Text = word And Button2.Text = word And Button3.Text = word Then ' 1 2 3
+            ' MsgWinName(plname)
+
+            status = True
+            '  comptescore += 1
+            Score.Text = CStr(comptescore)
+            btnunchecked()
+        ElseIf Button4.Text = word And Button5.Text = word And Button6.Text = word Then ' 4 5 6
+            '  MsgWinName(plname)
+            status = True
+            ' comptescore += 1
+            Score.Text = CStr(comptescore)
+            btnunchecked()
+        ElseIf Button7.Text = word And Button8.Text = word And Button9.Text = word Then '7 8 9
+            ' MsgWinName(plname)
+            status = True
+            '   comptescore += 1
+            Score.Text = CStr(comptescore)
+            btnunchecked()
+        ElseIf Button1.Text = word And Button4.Text = word And Button7.Text = word Then '1 4 7
+            '  MsgWinName(plname)
+            status = True
+            comptescore += 1
+            Score.Text = CStr(comptescore)
+            btnunchecked()
+        ElseIf Button2.Text = word And Button5.Text = word And Button8.Text = word Then '2 5 8
+            ' MsgWinName(plname)
+            status = True
+            '   comptescore += 1
+            Score.Text = CStr(comptescore)
+            btnunchecked()
+        ElseIf Button3.Text = word And Button6.Text = word And Button9.Text = word Then '3 6 9
+            ' MsgWinName(plname)
+            status = True
+            ' comptescore += 1
+            Score.Text = CStr(comptescore)
+            btnunchecked()
+        ElseIf Button1.Text = word And Button5.Text = word And Button9.Text = word Then '1 5 9
+            '  MsgWinName(plname)
+            status = True
+            'comptescore += 1
+            Score.Text = CStr(comptescore)
+            btnunchecked()
+        ElseIf Button3.Text = word And Button5.Text = word And Button7.Text = word Then '3 5  7
+            '  MsgWinName(plname)
+            status = True
+            'comptescore += 1
+            Score.Text = CStr(comptescore)
+            btnunchecked()
+        Else
+            status = False
+            ' Else
+            ' status = False
+            'Else
+            'MsgBox("Draw")
+            'comptescore += 1
+            'Score.Text = CStr(comptescore)
+        End If
+        '    While (status = True)
+        '    End While       '  Else
+        '    ' status = False
+        '    ' End If
+        '    'MsgBox("Draw")
+        '    'comptescore += 1
+        '    'Score.Text = CStr(comptescore)
+        '    'status = False
+        If status = True Then
+            playerwin(player)
+            MsgWinName(plname)
+        ElseIf status = False Then
+            MsgBox("draw")
+        Else
+
+        End If
+    End Sub
+    Public Sub Resetgame()
+        RadioButton1.Enabled = True
+        RadioButton1.Checked = False
+        RadioButton3.Enabled = True
+        status = False
+        CheckBox1.Checked = False
+        CheckBox2.Checked = False
+        CheckBox2.Enabled = True
+        CheckBox1.Enabled = True
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+        comptescore = 0
+    End Sub
+    Public Sub testchecked()
+        If CheckBox1.Checked = False And CheckBox2.Checked = False Then
+            MsgBox("Il faut cocher le  case")
+        End If
+    End Sub
+
+    Public Sub jeux()
+        If CheckBox1.Checked Then
+            'MsgBox("player 1")
+            player = 1
+            lblplayer.Text = "Player 1"
+            CheckBox2.Checked = True
+            CheckBox2.Enabled = True
+            CheckBox1.Checked = False
+            CheckBox2.Checked = True
+        ElseIf CheckBox2.Checked Then
+            'MsgBox("Player 2")
+            lblplayer.Text = "player 2"
+            player = 2
+            CheckBox2.Checked = True
+            CheckBox2.Enabled = True
+            CheckBox1.Checked = False
+            CheckBox2.Checked = False
+            CheckBox1.Checked = True
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        jeux()
+        If player = 1 Then
+            Button1.Text = word
+            Button1.Enabled = False
+
+        ElseIf player = 2 Then
+            Button1.Text = word
+            Button1.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        'If playerStatut = 0 Then
+        'Button1.Text = word 'X
+        'Button1.Enabled = False
+        ' Else
+        'Button1.Text = word 'O
+        'Button1.Enabled = False
+        '  End If
+        ' CheckBox1.Checked = False
+        ' CheckBox2.Checked = True
+        ' CheckBox2.Enabled = False
+        '  If player = 1 Then
+        ' CheckBox1.Enabled
+        '  End If
+        checkwin()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        jeux()
+        If player = 1 Then
+            Button2.Text = word
+            Button2.Enabled = False
+        ElseIf player = 2 Then
+            Button2.Text = word
+            Button2.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        checkwin()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        jeux()
+        If player = 1 Then
+            Button3.Text = word
+            Button3.Enabled = False
+
+        ElseIf player = 2 Then
+            Button3.Text = word
+            Button3.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        checkwin()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        jeux()
+        If player = 1 Then
+            Button4.Text = word
+            Button4.Enabled = False
+        ElseIf player = 2 Then
+            Button4.Text = word
+            Button4.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        checkwin()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        jeux()
+        If player = 1 Then
+            Button5.Text = word
+            Button5.Enabled = False
+        ElseIf player = 2 Then
+            Button5.Text = word
+            Button5.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        checkwin()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        jeux()
+        If player = 1 Then
+            Button6.Text = word
+            Button6.Enabled = False
+        ElseIf player = 2 Then
+            Button6.Text = word
+            Button6.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        checkwin()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        jeux()
+        If player = 1 Then
+            Button7.Text = word
+            Button7.Enabled = False
+        ElseIf player = 2 Then
+            Button7.Text = word
+            Button7.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        checkwin()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        jeux()
+        If player = 1 Then
+            Button8.Text = word
+            Button8.Enabled = False
+        ElseIf player = 2 Then
+            Button8.Text = word
+            Button8.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        checkwin()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        jeux()
+        If player = 1 Then
+            Button9.Text = word
+            Button9.Enabled = False
+        ElseIf player = 2 Then
+            Button9.Text = word
+            Button9.Enabled = False
+
+        Else
+            MsgBox("Erreur")
+        End If
+        checkwin()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            If player = 1 Then
+                word = "X"
+            Else
+                word = "O"
+            End If
+
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked Then
+            If player = 2 Then
+                word = "O"
+            Else
+                ' player = 1
+                word = "X"
+            End If
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked Then
+            RadioButton3.Enabled = False
+            RadioButton1.Enabled = False
+            CheckBox1.Checked = True
+            CheckBox1.Enabled = False
+            CheckBox2.Checked = True
+            CheckBox2.Enabled = False
+            player = 1
+            player = 2
+            'If player = 1 Then
+            '    CheckBox1.Enabled = True
+            '    CheckBox1.Checked = True
+            'Else
+            '    CheckBox2.Enabled = False
+            '    CheckBox2.Checked = False
+            'End If
+            'If player = 2 Then
+            '    CheckBox2.Enabled = True
+            '    CheckBox2.Checked = True
+            'Else
+            '    CheckBox1.Enabled = False
+            '    CheckBox1.Enabled = False
+            'End If
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Resetgame()
+    End Sub
+
+End Class
